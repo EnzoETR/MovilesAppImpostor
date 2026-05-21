07d073b1d7ff00209af1f25b27e83dc60788a81e
EnzoETR <enzo.torres@estudiantes.utec.edu.uy>
2026-05-21 19:43:52 -0300

    prueba

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,23 @@
+import {View,StyleSheet} from 'react-native';
+
+export default function IndexScreen(){
+
+
+
+    return (
+
+        <View style={styles.container}>
+
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#0aadad',
+        alignItems: 'center',  
+        justifyContent: 'center',
+    },
+});
